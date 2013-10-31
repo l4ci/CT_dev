3f3809c46456f2eb0cf3a96871ac141b242bbb62
Volker Otto <pim.volker@gmail.com>
2013-10-31 14:45:52 +0100

    big changes
    more color variations
    modified dropbox and button classes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -48,19 +48,19 @@ jQuery( document ).ready(function( $ ) {
 
 
   // Dropdown
-  $('.dropdown .dropdown-toggle').click(function(e){
+  $('.dpd .dpd-toggle').click(function(e){
 		e.preventDefault();
 
-		$(this).toggleClass('open');
+		$(this).toggleClass('dpd-open');
 		// Change icon if any
 		$(this).find('i.fa').toggleClass('fa-angle-down fa-angle-up');
 
 		// Toggle Class and Animate
-		$(this).next('.dropdown-menu').slideToggle(300, function(){
+		$(this).next('.dpd-menu').slideToggle(300, function(){
 				if ($(this).is(":visible") === false){
-					$(this).removeClass('open').addClass('closed').removeAttr('style');
+					$(this).removeClass('dpd-open').addClass('dpd-closed').removeAttr('style');
 				} else {
-					$(this).removeClass('closed').addClass('open').removeAttr('style');
+					$(this).removeClass('dpd-closed').addClass('dpd-open').removeAttr('style');
 				}
 			});
   });
