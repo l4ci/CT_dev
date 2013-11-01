f12d2f2d47cc4e56e75aa6708cda775fea498851
Volker Otto <pim.volker@gmail.com>
2013-11-01 08:52:25 +0100

    few changes
    added icon toggle
    added setting

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -86,6 +86,44 @@ jQuery( document ).ready(function( $ ) {
 	});
 
 
+	// Toggle Icons
+	$('.fa.toggle').click(function(){
+		$(this).removeClass('fa toggle');
+		var data	= 'fa toggle ' + $(this).data('toggle');
+		var old = $(this).attr('class');
+		$(this).attr('class', data).data('toggle',old);
+	});
+
+
+	// Setting
+  $('.setting .set-toggle').click(function(e){
+		e.preventDefault();
+
+		var parent = $(this).parents('.setting');
+		var show   = parent.find('.set-show');
+		var hide   = parent.find('.set-hide');
+		var btn = $(this);
+
+		btn.removeClass('active').find('i.fa').toggleClass('fa-plus fa-minus');
+
+		if (show.is(":visible") === false){
+			show.show();
+			hide.hide();
+			//show.show(300, function(){
+			//show.removeClass('set-closed').addClass('set-open').removeAttr('style');
+			//hide.removeClass('set-open').addClass('set-closed').removeAttr('style');
+			//});
+		}else{
+			hide.show();
+			show.hide();
+
+			//hide.show(300, function(){
+			//show.removeClass('set-open').addClass('set-closed').removeAttr('style');
+			//hide.removeClass('set-closed').addClass('set-open').removeAttr('style');
+			//});
+		}
+	});
+
   // Global Resize Function
 	$(window).resize(function() {
 
