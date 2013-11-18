81dc30f56de5d0075736c9939bbe569776e7c872
Volker Otto <pim.volker@gmail.com>
2013-11-18 18:05:41 +0100

    changes

--- build/js/ext/_fixedtotop.js
+++ build/js/ext/_fixedtotop.js
@@ -3,38 +3,17 @@
  *
  * fixes the header to top of the page when scrolling down
  **/
-// jQuery(document).ready(function($) {
-
-// 	var fixedtotop = $('.fixedtotop'),
-// 			pos = fixedtotop.offset(),
-// 			height = fixedtotop.outerHeight(),
-// 			until = fixedtotop.data('scroll-next');
-
-// 	$(window).scroll(function() {
-// 		if($(this).scrollTop() > (pos.top) && fixedtotop.css('position') === 'static') {
-// 			fixedtotop.addClass('fixed').before( "<div class='fixtotop_placeholder' style='height: " + (height * 1.6 ) +"px'></div>");
-// 		}	else if($(this).scrollTop() <= pos.top && fixedtotop.hasClass('fixed')){
-// 			fixedtotop.removeClass('fixed');
-// 			$('.fixtotop_placeholder').remove();
-// 		} else if ($(this.scrollTop() >= until)){
-// 			fixedtotop.removeClass('fixed');
-// 			$('.fixtotop_placeholder').remove();
-// 		}
-// 	});
-// });
-
-
 jQuery(document).ready(function($) {
 
 	function getScrollY(){
 		return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 	}
 
-	var lastKnownScrollY 	= 0,
-			tolerance        	= 10,
-			offset           	= 100,
-			fixedelem 				= $('.fixedtotop'),
-			height 						=	fixedelem.outerHeight();
+	var lastKnownScrollY = 0,
+			tolerance = 5,
+			offset = 60,
+			fixedelem = $('.fixedtotop'),
+			height =	fixedelem.outerHeight();
 
 
 
@@ -42,25 +21,32 @@ jQuery(document).ready(function($) {
 
 		var currentScrollY = getScrollY();
 
-    if(currentScrollY < 0) { // Ignore bouncy scrolling in OSX
+		// Ignore bouncy scrolling in OSX
+    if(currentScrollY < 0) {
       return;
     }
 
-    if(currentScrollY <= (offset + height)){
-    	fixedelem.removeClass('fixedtotop--pinned fixedtotop--unpinned');
-    }
+    // Only animate when further down than elements height and offset
+    if(currentScrollY >= height){
 
-    if (Math.abs(currentScrollY-lastKnownScrollY) >= tolerance) {
+			// only animate if within tolerance movement of scroll
+			if (Math.abs(currentScrollY-lastKnownScrollY) >= tolerance) {
 
-      if (currentScrollY > lastKnownScrollY && currentScrollY >= offset) {
-				fixedelem.removeClass('fixedtotop--pinned');
-				fixedelem.addClass('fixedtotop--unpinned');
-      } else if (currentScrollY < lastKnownScrollY) {
-				fixedelem.removeClass('fixedtotop--unpinned');
-				fixedelem.addClass('fixedtotop--pinned');
-      }
+				// Hide
+				if (currentScrollY > lastKnownScrollY && currentScrollY >= offset) {
+					fixedelem.removeClass('slideInDown').addClass('slideOutUp');
 
-    }
+				// Show
+				} else if (currentScrollY < lastKnownScrollY) {
+					fixedelem.removeClass('slideOutUp').addClass('slideInDown');
+				}
+			}
+		// show element
+		}else{
+			fixedelem.removeClass('slideOutUp slideInDown');
+		}
+
+
     lastKnownScrollY = currentScrollY;
   });
 
