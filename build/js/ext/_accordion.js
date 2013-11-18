268dd3314b8dbaa69343797782bbd8a2f9d5a897
Volker Otto <pim.volker@gmail.com>
2013-11-18 09:00:06 +0100

    wrap in functions

--- build/js/ext/_accordion.js
+++ build/js/ext/_accordion.js
@@ -1,3 +1,40 @@
+// var accordion = (function () {
+//     var s; // private alias to settings
+
+//     return {
+//         settings: {
+//             accordionDiv: $(".accordion > dt"),
+//             allPanels: $(this).parents('.accordion').find('dd'),
+//             target = $(this).next()
+//         },
+
+//         init: function() {
+//             s = this.settings;
+//             this.bindUIActions();
+//         },
+
+//         bindUIActions: function() {
+//             s.accordionDiv.on("click", function() {
+//                 accordionDiv.togglePanel();
+//             });
+//         },
+
+//         togglePanel: function() {
+//         	e.preventDefault();
+
+// 					if(!target.hasClass('active')){
+// 						allPanels.slideUp(300,function(){
+// 							$(this).removeClass('active').removeAttr('style');
+// 						});
+// 						target.slideDown(300,function(){
+// 							$(this).addClass('active').removeAttr('style');
+// 						});
+// 					}
+//         }
+
+//     };
+// })();
+
 jQuery(document).ready(function($) {
 	// Accordion
 	$('.accordion > dt').click(function(e) {
